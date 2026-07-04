1760aaad789c4660eaeaed80101937b2f7e11a9b
bramanproject <81703912+bramanproject@users.noreply.github.com>
2026-07-04 15:34:15 +0300

    Update indicators.py

--- core/indicators.py
+++ core/indicators.py
@@ -1,41 +1,33 @@
 import pandas as pd
 
 
-def ema(series: pd.Series, period: int) -> pd.Series:
+def ema(series: pd.Series, period: int):
     return series.ewm(span=period, adjust=False).mean()
 
 
-def rsi(series: pd.Series, period: int = 14) -> pd.Series:
+def rsi(series: pd.Series, period: int = 14):
     delta = series.diff()
 
-    gain = delta.clip(lower=0)
-    loss = -delta.clip(upper=0)
+    gain = (delta.where(delta > 0, 0)).rolling(window=period).mean()
+    loss = (-delta.where(delta < 0, 0)).rolling(window=period).mean()
 
-    avg_gain = gain.rolling(period).mean()
-    avg_loss = loss.rolling(period).mean()
-
-    rs = avg_gain / avg_loss
-
+    rs = gain / loss
     return 100 - (100 / (1 + rs))
 
 
 def atr(df: pd.DataFrame, period: int = 14):
+    high = df["high"]
+    low = df["low"]
+    close = df["close"]
 
-    high = df["High"]
-    low = df["Low"]
-    close = df["Close"]
-
-    tr1 = high - low
-    tr2 = (high - close.shift()).abs()
-    tr3 = (low - close.shift()).abs()
-
-    tr = pd.concat([tr1, tr2, tr3], axis=1).max(axis=1)
+    tr = pd.concat([
+        high - low,
+        (high - close.shift()).abs(),
+        (low - close.shift()).abs()
+    ], axis=1).max(axis=1)
 
     return tr.rolling(period).mean()
 
 
 def volume_ratio(volume: pd.Series, period: int = 20):
-
-    avg = volume.rolling(period).mean()
-
-    return volume / avg
+    return volume / volume.rolling(period).mean()
