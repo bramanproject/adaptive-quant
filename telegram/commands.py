0d530dd1de4afad8192c3a8314882d5f1200da73
bramanproject <81703912+bramanproject@users.noreply.github.com>
2026-07-04 11:38:40 +0300

    Create commands.py

---
+++ telegram/commands.py
@@ -0,0 +1,25 @@
+# telegram/commands.py
+
+START_TEXT = """
+Adaptive Quant
+
+/send TICKER
+
+Example:
+
+/send AAPL
+"""
+
+HELP_TEXT = """
+Commands
+
+/start
+
+/help
+
+/send NVDA
+
+/send TSLA
+
+/send MSFT
+"""
